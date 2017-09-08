35ff3532a45221a5e608fb911b3722ca85160f9c
hym <1349456160@qq.com>
2017-09-08 08:45:46 +0800

    隐式intent启动活动的初步实践

--- app/src/main/java/com/example/lenovo/intent/FirstActivity.java
+++ app/src/main/java/com/example/lenovo/intent/FirstActivity.java
@@ -1,13 +1,34 @@
 package com.example.lenovo.intent;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class FirstActivity extends AppCompatActivity {
+public class FirstActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first);
+        Button button1=(Button)findViewById(R.id.button1);
+        Button button2=(Button)findViewById(R.id.button2);
+        Button button3=(Button)findViewById(R.id.button3);
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.button1:
+                Intent intent=new Intent("com.exmaple.activitytest.ACTION_START");
+                startActivity(intent);
+                break;
+            default:
+                break;
+        }
     }
 }
